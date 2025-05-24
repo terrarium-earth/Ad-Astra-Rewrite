8f4f35e986a21298e7fb6156dc04ea42763ee056
Alex Nijjar <alexander.nijjar@icloud.com>
2025-05-24 02:28:15 -0700

    Set up cloche

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+  kotlin("jvm") version "2.1.21"
+  id("com.google.devtools.ksp") version "2.1.21-2.0.1"
+  id("earth.terrarium.cloche") version "0.9.17"
+}
+
+group = "earth.terrarium.adastra"
+
+version = "2.0.0"
+
+repositories {
+  cloche {
+    main()
+    librariesMinecraft()
+    mavenNeoforged()
+  }
+}
+
+cloche {
+  metadata {
+    modId = "adastra"
+    name = "Ad Astra"
+    description = "Cool space mod"
+    license = "MIT"
+    author("Alex Nijjar")
+  }
+
+  minecraftVersion = "1.21.5"
+
+  singleTarget {
+    neoforge {
+      loaderVersion = "21.5.67-beta"
+      mixins.from("adastra.mixins.json")
+
+      data()
+      test()
+
+      runs {
+        server()
+        client()
+        data()
+        test()
+      }
+    }
+  }
+
+  mappings {
+    official()
+    parchment("2025.04.19")
+  }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "adastra"
+
+pluginManagement {
+  repositories {
+    maven(url = "https://maven.msrandom.net/repository/cloche")
+
+    gradlePluginPortal()
+  }
+}
